e43b0fab24518bc787375bc57929d84b88461a21
jaredwines <jared.wines@pricefx.com>
2023-07-31 16:17:28 -0700

    Change IP.

--- src/components/deployment-form/submitCommand.ts
+++ src/components/deployment-form/submitCommand.ts
@@ -2,7 +2,7 @@ export const submitCommand = async (data: Inputs) => {
     if (!data.project || !data.action) {
         return
     }
-    let url = 'http://192.168.0.216:5555/' + data.project + '/' + data.action;
+    let url = 'http://0.0.0.0:5555/' + data.project + '/' + data.action;
 
     if (data.branch !== 'none') {
         url += '/' + data.branch;
