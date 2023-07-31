9873bf9a1970183db3ae0a2cfa87163a91c0a9f0
Fransheska Colon <franc@alum.mit.edu>
2023-07-30 20:46:38 -0700

    update light theme with desired background

--- src/shared/styles/theme.ts
+++ src/shared/styles/theme.ts
@@ -1,6 +1,12 @@
 import {createTheme} from "@mui/material";
 
-export const lightTheme = createTheme();
+export const lightTheme = createTheme({
+    palette: {
+        background: {
+            default: '#f8f9fc'
+        }
+    }
+});
 export const darkTheme = createTheme({
     palette: {
         mode: 'dark',
